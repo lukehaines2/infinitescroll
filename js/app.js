2525e8e6bc53cd978a160c9af4ea0b1e4b3ec936
Luke Haines <lukehaines2@gmail.com>
2020-11-21 18:18:22 +0000

    add eventListeners and fix naming on quotes

--- js/app.js
+++ js/app.js
@@ -1,74 +1,95 @@
-const quotesEl = document.querySelector(".quotes");
-const loaderEl = document.querySelector(".loader");
+(function () {
 
-const getQuotes = async (page, limit) => {
-  const API_URL = `https://api.javascripttutorial.net/v1/quotes/?page=${page}&limit=${limit}`;
-  const response = await fetch(API_URL);
-  //  Handle 404
-  if (!response.status === 200) {
-    throw new Error(`Error: ${response.status}`);
+  const quotesEl = document.querySelector(".quotes");
+  const loaderEl = document.querySelector(".loader");
+
+  const getQuotes = async (page, limit) => {
+    const API_URL = `https://api.javascripttutorial.net/v1/quotes/?page=${page}&limit=${limit}`;
+    const response = await fetch(API_URL);
+    //  Handle 404
+    if (!response.status === 200) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    return await response.json();
   }
-  return await response.json();
-}
 
 
-const renderQuotes = (quotes) => {
-  quotes.forEach(quote => {
-    const quoteEl = document.createElement("blockquote");
-    quoteEl.classList.add("quote");
+  const renderQuotes = (quotes) => {
+    quotes.forEach(quote => {
+      const quoteEl = document.createElement("blockquote");
+      quoteEl.classList.add("quote");
 
-    quoteEl.innerHTML = `
+      quoteEl.innerHTML = `
       <span>${quote.id})</span>
       ${quote.quote}
       <footer>${quote.author}</footer>
     `;
 
-    quotes.appendChild(quoteEl);
-  });
-}
+      quotesEl.appendChild(quoteEl);
+    });
+  }
 
 
-const showLoader = () => {
-  loaderEl.classList.add("show");
-}
+  const showLoader = () => {
+    loaderEl.classList.add("show");
+  }
 
 
-const hideLoader = () => {
-  loaderEl.classList.remove("show");
-}
+  const hideLoader = () => {
+    loaderEl.classList.remove("show");
+  }
 
 
-let currentPage = 1;
-const limit = 10;
-let total = 0;
+  let currentPage = 1;
+  const limit = 10;
+  let total = 0;
 
-// Eval is it the first call or not?
-const hasMoreQuotes = (page, limit, total) => {
-  const startIndex = (page - 1) * limit + 1;
-  return total === 0 || startIndex < total;
-};
+  // Eval is it the first call or not?
+  const hasMoreQuotes = (page, limit, total) => {
+    const startIndex = (page - 1) * limit + 1;
+    return total === 0 || startIndex < total;
+  };
 
 
-// Load quotes
-const loadQuotes = async (page, limit) => {
+  const loadQuotes = async (page, limit) => {
+    // Show Loader on FE
+    showLoader();
 
-  // Show loader
-  showLoader();
-
-  // Timeout hack to ensure loader is displayed for own visual benefit
-  setTimeout(async () => {
-    try {
-      if (hasMoreQuotes(page, limit, total)) {
-        // Call API
-        const response = await getQuotes(page, limit);
-        showQuotes(response.data);
-        total = response.total;
+    // Timeout hack to ensure loader is displayed for own visual benefit
+    setTimeout(async () => {
+      try {
+        if (hasMoreQuotes(page, limit, total)) {
+          // Call API
+          const response = await getQuotes(page, limit);
+          renderQuotes(response.data);
+          total = response.total;
+        }
+      } catch (e) {
+        console.error(e.message);
+      } finally {
+        hideLoader();
       }
-    } catch (e) {
-      console.error(error.message);
-      console.log(error.message);
-    } finally {
-      hideLoader();
+    }, 500);
+  };
+
+
+  window.addEventListener('scroll', () => {
+    const {
+      scrollTop,
+      scrollHeight,
+      clientHeight
+    } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 5 &&
+      hasMoreQuotes(currentPage, limit, total)) {
+      currentPage++;
+      loadQuotes(currentPage, limit);
     }
-  }, 500);
-};
+  }, {
+    passive: true
+  });
+
+
+  loadQuotes(currentPage, limit);
+
+})();
