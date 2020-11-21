02ccbdb44b2fde123f45c9239e76fba34df36667
Luke Haines <lukehaines2@gmail.com>
2020-11-21 18:11:31 +0000

    add loading functionality

--- js/app.js
+++ js/app.js
@@ -4,15 +4,12 @@ const loaderEl = document.querySelector(".loader");
 const getQuotes = async (page, limit) => {
   const API_URL = `https://api.javascripttutorial.net/v1/quotes/?page=${page}&limit=${limit}`;
   const response = await fetch(API_URL);
-  console.log('L7 === response', response);
   //  Handle 404
   if (!response.status === 200) {
     throw new Error(`Error: ${response.status}`);
   }
   return await response.json();
 }
-
-const myQuotes = getQuotes(1, 10);
 
 
 const renderQuotes = (quotes) => {
@@ -29,3 +26,49 @@ const renderQuotes = (quotes) => {
     quotes.appendChild(quoteEl);
   });
 }
+
+
+const showLoader = () => {
+  loaderEl.classList.add("show");
+}
+
+
+const hideLoader = () => {
+  loaderEl.classList.remove("show");
+}
+
+
+let currentPage = 1;
+const limit = 10;
+let total = 0;
+
+// Eval is it the first call or not?
+const hasMoreQuotes = (page, limit, total) => {
+  const startIndex = (page - 1) * limit + 1;
+  return total === 0 || startIndex < total;
+};
+
+
+// Load quotes
+const loadQuotes = async (page, limit) => {
+
+  // Show loader
+  showLoader();
+
+  // Timeout hack to ensure loader is displayed for own visual benefit
+  setTimeout(async () => {
+    try {
+      if (hasMoreQuotes(page, limit, total)) {
+        // Call API
+        const response = await getQuotes(page, limit);
+        showQuotes(response.data);
+        total = response.total;
+      }
+    } catch (e) {
+      console.error(error.message);
+      console.log(error.message);
+    } finally {
+      hideLoader();
+    }
+  }, 500);
+};
