6be735fc84bee4047539a2d40aa0cfc70919f069
Luke Haines <lukehaines2@gmail.com>
2020-11-21 16:40:53 +0000

    add render quote fnct

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const loaderEl = document.querySelector(".loader");
 const getQuotes = async (page, limit) => {
   const API_URL = `https://api.javascripttutorial.net/v1/quotes/?page=${page}&limit=${limit}`;
   const response = await fetch(API_URL);
-  console.log('L7 === response', await response.json());
+  console.log('L7 === response', response);
   //  Handle 404
   if (!response.status === 200) {
     throw new Error(`Error: ${response.status}`);
@@ -12,4 +12,20 @@ const getQuotes = async (page, limit) => {
   return await response.json();
 }
 
-getQuotes(1, 10);
+const myQuotes = getQuotes(1, 10);
+
+
+const renderQuotes = (quotes) => {
+  quotes.forEach(quote => {
+    const quoteEl = document.createElement("blockquote");
+    quoteEl.classList.add("quote");
+
+    quoteEl.innerHTML = `
+      <span>${quote.id})</span>
+      ${quote.quote}
+      <footer>${quote.author}</footer>
+    `;
+
+    quotes.appendChild(quoteEl);
+  });
+}
